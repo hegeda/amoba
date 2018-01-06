e47429e467e3dffe8cc698641d129729da1ce0ae
hegeda <davidhege9@gmail.com>
2018-01-06 13:22:36 +0100

    Board and input is ready.

--- amoba.py
+++ amoba.py
@@ -1,34 +1,92 @@
 # coding=utf-8
 print ('Amőba játék')
-j='X'
+j = 'X'
+tbl = {'a1': "", 'a2': "", 'a3': "", 'b1': "", 'b2': "", 'b3': "", 'c1': "", 'c2': "", 'c3': ""}
+vege=False
 
-def tabla_init(a1, a2, a3, b1, b2, b3, c1, c2, c3):
-    tbl = {'a1': a1, 'a2': a2, 'a3': a3, 'b1': b1, 'b2': b2, 'b3': b3, 'c1': c1, 'c2': c2, 'c3': c3}
+
+def tabla(mezo, ertek):
+    if mezo == 'I':
+        ertek = "-"
+        for i in tbl.iterkeys():
+            tbl[i] = ertek
+    elif mezo == 'a1':
+        tbl['a1'] = ertek
+    elif mezo == 'a2':
+        tbl['a2'] = ertek
+    elif mezo == 'a3':
+        tbl['a3'] = ertek
+    elif mezo == 'b1':
+        tbl['b1'] = ertek
+    elif mezo == 'b2':
+        tbl['b2'] = ertek
+    elif mezo == 'b3':
+        tbl['b3'] = ertek
+    elif mezo == 'c1':
+        tbl['c1'] = ertek
+    elif mezo == 'c2':
+        tbl['c2'] = ertek
+    elif mezo == 'c3':
+        tbl['c3'] = ertek
+    else:
+        print 'Érvénytelen koordináta! Adj újat!'
+
     print tbl['a1'], '|', tbl['a2'], '|', tbl['a3']
     print tbl['b1'], '|', tbl['b2'], '|', tbl['b3']
     print tbl['c1'], '|', tbl['c2'], '|', tbl['c3']
+    return ""
 
 
-def tabla(a1, a2, a3, b1, b2, b3, c1, c2, c3):
-    pass
-
-def valtas(j):
+def valtas(valt):
     if j == 'X':
-        valt='O'
+        valt = 'O'
         print'O jön'
     else:
-        valt='X'
+        valt = 'X'
         print'X jön'
     return valt
 
+
 def bevitel():
-    bev = input("Koordináták (pl.: a2): ")
+    bev = raw_input("Koordináták (pl.: a2): ")
     return bev
 
+
 def ertekel():
+    if tbl['a1'] == tbl['a2'] == tbl['a3']:
+        print 'nyert'
+        vege=True
+    elif tbl['b1'] == tbl['b2'] == tbl['b3']:
+        print 'nyert'
+        vege = True
+    elif tbl['c1'] == tbl['c2'] == tbl['c3']:
+        print 'nyert'
+        vege = True
+    elif tbl['a1'] == tbl['b1'] == tbl['c1']:
+        print 'nyert'
+        vege = True
+    elif tbl['a2'] == tbl['b2'] == tbl['c2']:
+        print 'nyert'
+        vege = True
+    elif tbl['a3'] == tbl['b3'] == tbl['c3']:
+        print 'nyert'
+        vege = True
+    elif tbl['a1'] == tbl['b2'] == tbl['c3']:
+        print 'nyert'
+        vege = True
+    elif tbl['a3'] == tbl['b2'] == tbl['c1']:
+        print 'nyert'
+        vege = True
+    else:
+        return ""
 
-    valtas()
+
+def tabla_init():
+    print tabla('I', '-')
 
 
+tabla_init()
+while (vege==False):
+    tabla(bevitel(), j)
+    ertekel()
 
-print tabla_init('-', '-', '-', '-', '-', '-', '-', '-', '-')

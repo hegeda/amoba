998f3f363d41f769925aab78a35ed106a6a37a61
hegeda <davidhege9@gmail.com>
2018-01-06 16:04:10 +0100

    restart implemented

--- amoba.py
+++ amoba.py
@@ -4,9 +4,10 @@ import os
 clear = lambda: os.system('clear')
 j = 'X'
 tbl = {'a1': "", 'a2': "", 'a3': "", 'b1': "", 'b2': "", 'b3': "", 'c1': "", 'c2': "", 'c3': ""}
-vege=False
-nevalt=False
-lep=0
+vege = False
+nevalt = False
+lep = 0
+
 
 def tabla(mezo, ertek):
     global nevalt
@@ -19,7 +20,7 @@ def tabla(mezo, ertek):
     elif mezo == 'a1' and tbl['a1'] == "-":
         tbl['a1'] = ertek
     elif mezo == 'a2' and tbl['a2'] == "-":
-        tbl['a2'] =  ertek
+        tbl['a2'] = ertek
     elif mezo == 'a3' and tbl['a3'] == "-":
         tbl['a3'] = ertek
     elif mezo == 'b1' and tbl['b1'] == "-":
@@ -36,7 +37,7 @@ def tabla(mezo, ertek):
         tbl['c3'] = ertek
     else:
         print 'Érvénytelen koordináta! Adj újat!'
-        nevalt=True
+        nevalt = True
     print '     1   2   3'
     print 'a   ', tbl['a1'], '|', tbl['a2'], '|', tbl['a3']
     print 'b   ', tbl['b1'], '|', tbl['b2'], '|', tbl['b3']
@@ -52,7 +53,7 @@ def valtas():
     if nevalt:
         print j, ' jön'
         nevalt = False
-        return""
+        return ""
     else:
         if j == 'X':
             j = 'O'
@@ -66,7 +67,7 @@ def valtas():
 
 
 def bevitel():
-    bev = raw_input("Koordináták (pl.: a2): ")
+    bev = raw_input("Koordináták (pl.: a2, újra: r ): ")
     return bev
 
 
@@ -75,44 +76,54 @@ def ertekel():
     global lep
     if tbl['a1'] <> '-' and tbl['a1'] == tbl['a2'] == tbl['a3']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['b1'] <> '-' and tbl['b1'] == tbl['b2'] == tbl['b3']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['c1'] <> '-' and tbl['c1'] == tbl['c2'] == tbl['c3']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['a1'] <> '-' and tbl['a1'] == tbl['b1'] == tbl['c1']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['a2'] <> '-' and tbl['a2'] == tbl['b2'] == tbl['c2']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['a3'] <> '-' and tbl['a3'] == tbl['b3'] == tbl['c3']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['a1'] <> '-' and tbl['a1'] == tbl['b2'] == tbl['c3']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif tbl['a3'] <> '-' and tbl['a3'] == tbl['b2'] == tbl['c1']:
         print 'Nyert', j
-        vege = True
+        ujra()
     elif lep == 8:
         print 'Döntetlen'
-        vege = True
+        ujra()
     else:
         valtas()
         return False
 
 
+def ujra():
+    global vege
+    re = raw_input("Új játék?: (i/n)")
+    if re == 'i':
+        tabla_init()
+    else:
+        vege = True
+
+
 def tabla_init():
+    global lep
+    global vege
     tabla('I', '-')
-    vege=False
+    lep = 0
+    vege = False
 
 
 tabla_init()
-while vege==False:
+while vege == False:
     tabla(bevitel(), j)
     ertekel()
-
-
